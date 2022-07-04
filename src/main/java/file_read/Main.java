f909b604bbec0510e90cb2d77e0343cfc0f77443
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-07-04 21:37:15 +0100

    made some fix

--- src/main/java/file_read/Main.java
+++ src/main/java/file_read/Main.java
@@ -15,9 +15,10 @@ public class Main {
             System.out.println("*********List of Products*********");
             printProductList(productService.getProductList());
             System.out.println();
-            System.out.println("==========================================================");
             System.out.println(productService.createProducts(file, productList));
 //            productService.getProductList().forEach(System.out::println);
+            System.out.println("\n==========================================================");
+            System.out.println("*********List as Total Unique Products*********");
             printProductList(productService.createUniqueProducts());
         } catch (IOException e) {
             throw new RuntimeException(e);
